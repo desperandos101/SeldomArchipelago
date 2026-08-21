0537252a2bfa562a8178b9e66c139a0082a5b3d9
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2026-08-21 17:09:09 -0400

    ap0.6.200

--- Systems/ArchipelagoSystem.cs
+++ Systems/ArchipelagoSystem.cs
@@ -40,7 +40,7 @@ namespace SeldomArchipelagoBeta.Systems
 {
     class ArchipelagoSystem : ModSystem
     {
-        public readonly Version APversion = new Version(0, 6, 100);
+        public readonly Version APversion = new Version(0, 6, 200);
         public const string APWorldName = "Terraria Beta";
         // Data that's reset between worlds
         public class WorldState : TagSerializable
